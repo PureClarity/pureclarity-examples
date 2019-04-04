92dd12fd1f59b940b4c65f0141edbc68f6023fe2
Philip Bowler <philip.bowler@pureclarity.com>
2019-04-04 16:04:53 +0100

    Added the link to a working carousel codepen for help with implementing PureClarity

---
+++ Carousel/Carousel.js
@@ -0,0 +1,8 @@
+/**
+ *  PureClarity 
+ * 
+ *  Carousel CodePen Example
+ */
+
+// Follow this link to our CodePen where you can see a working example of a getzone call to get a carousel:
+// https://codepen.io/anon/pen/QPNWgv?editors=1000
