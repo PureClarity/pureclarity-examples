228469119ac0d2ac0c54e571336604829c748878
Philip Bowler <philip.bowler@pureclarity.com>
2019-04-04 12:11:31 +0100

    Changed the page that the product example displays on from the homepage to the product page

--- zone-examples/GetZoneExamples.js
+++ zone-examples/GetZoneExamples.js
@@ -14,8 +14,8 @@ _pc("getzone", {zones: ["HP-01, HP-02"]}, function(err, result) {
     // err is null if all ok
 });
 
-// Example of a getzone call for a single zone with a product context on the homepage
-_pc("getzone", {zone: "HP-01", product_id: "P12345"}, function(err, result) {
+// Example of a getzone call for a single zone with a product context on the Product Page
+_pc("getzone", {zone: "PP-01", product_id: "P12345"}, function(err, result) {
     // err is null if all ok
     // result is a hashmap is IDs with HTML 
 });
@@ -23,7 +23,7 @@ _pc("getzone", {zone: "HP-01", product_id: "P12345"}, function(err, result) {
 // Example of a getzone call for multiple zones with a product context on the Product page
 _pc('getzone', { zones: ['PP-01', 'PP-02'], product_id: 'P12345' }, function(err, result) {
     // err is null if all ok
-    // result is a hashmap of IDs with Html (eg. var html = result['HP-01'])
+    // result is a hashmap of IDs with Html (eg. var html = result['PP-01'])
 });
 
 // Example of a getzone call for a single zone with an autocomplete term context
